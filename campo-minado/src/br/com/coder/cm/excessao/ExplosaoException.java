3886bb9f4880e9853daa6b904fbf1c5d2c806c11
Ellen Melo <ellen.melo156@gmail.com>
2023-01-26 00:14:04 -0300

    Delete campo-minado directory

--- campo-minado/src/br/com/coder/cm/excessao/ExplosaoException.java
+++
@@ -1,5 +0,0 @@
-package br.com.coder.cm.excessao;
-
-public class ExplosaoException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-}
--- campo-minado/src/br/com/coder/cm/excessao/SairException.java
+++
@@ -1,6 +0,0 @@
-package br.com.coder.cm.excessao;
-
-public class SairException extends RuntimeException {
-
-	private static final long serialVersionUID = 1;
-}
